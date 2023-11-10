2aa857904ebe83e960a883694152c8be4ecf663c
igorir3 <97892348+igorir3@users.noreply.github.com>
2023-11-10 22:07:25 +0300

    Alpha 2.0

--- main.py
+++ main.py
@@ -1,42 +1,94 @@
 import time
-import pyautogui as pg
+from pynput.keyboard import Key, Controller
 import mss
-import numpy as np
 import mss.tools
-import keyboard
+import numpy as np
+import json
+import os.path
 
-INLG = []
+keyboard = Controller()
+debug_color_bool = False
+
+INLG = None
 def debug_color(inl):
     global INLG
     if inl == INLG:
         pass
     else:
-        print(inl)
+        if abs(inl[0] - inl[1]) > 10 and abs(inl[1] - inl[2]) > 10 and abs(inl[2] - inl[0]) > 10:
+            print(inl)
         INLG = inl
-
-monitor = {
-    "left" : 950,
-    "top" : 1000,
-    "width" : 1,
-    "height" : 1
-}
 while True:
-    if keyboard.is_pressed('esc'):
+    if not(os.path.exists("settings.json")):
+        print("settings.json not found, creation...")
+        classical_dict = {"monitor" : [1920, 1080], "coordinatesratio" : True, "coordinates" : [973, 920], "buttons": {"d":[164, 116, 21], "f":[10, 99, 196], "j":[19, 36, 164], "k":[82, 36, 141]}, "outputcolor" : False}
+        with open("settings.json", "w+") as jsonfile:
+            json.dump(classical_dict, jsonfile, sort_keys=True, indent=4)
+        print("settings.json is created!")
+        input("Check the settings and press Enter to continue")
+    else:
         break
+
+with open("settings.json", "r") as jsonfile:
+    settdict = json.load(jsonfile)
+
+if settdict["outputcolor"]:
+    debug_color_bool = True
+
+if settdict["coordinatesratio"] == True and settdict["monitor"] == [1920, 1080]:
+    monitor = {
+        "left" : 973,
+        "top" : 920,
+        "width" : 1,
+        "height" : 1
+    }
+elif settdict["coordinatesratio"] == True:
+    monitor = {
+        "left" : settdict["monitor"][0] * (1920 / 973),
+        "top" : settdict["monitor"][1] * (1080 / 920),
+        "width" : 1,
+        "height" : 1
+    }
+else:
+    monitor = {
+        "left" : settdict["coordinates"][0],
+        "top" : settdict["coordinates"][1],
+        "width" : 1,
+        "height" : 1
+    }
+
+buttons = []
+buttons_color = []
+buttdict = settdict["buttons"]
+for x in buttdict:
+    buttons.append(x)
+    buttons_color.append(buttdict[x])
+
+button = None
+log_buttons = ""
+log_buttons_count = 0
+
+print("Bot set up and run!")
+while True:
     with mss.mss() as sct:
         img = sct.grab(monitor)
         img_array = np.array(img)
-        color = [img_array[0][0][0], img_array[0][0][1], img_array[0][0][2]]
-        if color == [12, 112, 222]:
-            pg.press('f')
-            print("f")
-        elif color == [186, 131, 24]:
-            pg.press('d')
-            print("d")
-        elif color == [21, 41, 186]:
-            pg.press('j')
-            print("j")
-        elif color == [93, 41, 160]:
-            pg.press('k')
-            print("k")
-        #debug_color(color)
+    color = [img_array[0][0][0], img_array[0][0][1], img_array[0][0][2]]
+    if color in buttons_color and button == None: button = buttons[buttons_color.index(color)]
+    if button != None:
+        keyboard.press(button)
+        time.sleep(0.05)
+        keyboard.release(button)
+        
+        log_buttons = log_buttons + button
+        if log_buttons_count < 25:
+            print(log_buttons, end='\r')
+            log_buttons_count = log_buttons_count + 1
+        else:
+            print(log_buttons)
+            log_buttons = ""
+            log_buttons_count = 0
+
+        button = None
+    if debug_color_bool == True:
+        debug_color(color)
